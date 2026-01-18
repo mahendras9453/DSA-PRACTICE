5df54d723944246eda98c8b2728091207d01ab0b
mahendras9453 <patelmahendra9152@gmail.com>
2026-01-18 19:15:23 +0530

    majorityelement(P=27)

---
+++ majorityelement.java
@@ -0,0 +1,20 @@
+// leetcode problem (169) majority e;ement in the array
+class majorityelement {
+    public int majorityElement(int[] nums) {
+        int cand=0;
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(count==0){
+                cand=nums[i];
+            }
+            if(cand==nums[i]){
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+        return cand;
+    }
+}
