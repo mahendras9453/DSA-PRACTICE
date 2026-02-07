4c16f1fb14831a80eaef21a0f63c30d7dd00dae5
mahendras9453 <patelmahendra9152@gmail.com>
2026-02-07 20:36:41 +0530

    Maxconsecutive1's(P=52)

---
+++ max1inbinaryarr.java
@@ -0,0 +1,21 @@
+// leetcode problem (485) return the max consecutive ones in binary array
+class max1inbinaryarr {
+    public int findMaxConsecutiveOnes(int[] nums) {
+     
+        int max=0;
+        int count=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]==1) {
+                count++;
+                max=Math.max(max,count);
+                
+            }
+         
+                else count=0;
+            
+                
+        }
+        return max;
+    }
+}}
