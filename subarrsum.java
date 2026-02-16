e50c1062097645ec11a0b6bc033c31eda193ad2f
mahendras9453 <patelmahendra9152@gmail.com>
2026-02-16 18:15:53 +0530

    subarrsumequal(P=62)

---
+++ subarrsum.java
@@ -0,0 +1,22 @@
+// leetcode problem (560) return subarray sum equal to k
+
+class subarrsum {
+    public int subarraySum(int[] nums, int k) {
+        int n= nums.length;
+       int count=0;
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=i;j<n;j++){
+             
+                    sum+=nums[j];
+                    if(sum==k) {
+                    count++;
+                    
+                    }
+            }
+         
+        }
+        return count;
+        
+    }
+}
