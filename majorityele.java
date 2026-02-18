d540dab8abe467e21e69710acb772c79818bdb61
mahendras9453 <patelmahendra9152@gmail.com>
2026-02-18 21:31:19 +0530

    Majoritylele-n/3(P=65)

---
+++ majorityele.java
@@ -0,0 +1,42 @@
+//leetcode problem (229) return the majority elements greater than (n/3)
+import java .util.*;
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int n = nums.length;
+        int candidate1 = 0, candidate2 = 0;
+        int count1 = 0, count2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+
+        count1 = 0;
+        count2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == candidate1) count1++;
+            else if (nums[i] == candidate2) count2++;
+        }
+
+  
+        List<Integer> result = new ArrayList<>();
+        if (count1 > n / 3) result.add(candidate1);
+        if (count2 > n / 3) result.add(candidate2);
+
+        return result;
+    }
+}
