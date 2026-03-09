6d3c91cb8de642426e0504c8b659a01ded9ce64a
mahendras9453 <patelmahendra9152@gmail.com>
2026-03-09 19:56:21 +0530

    lowerbound(P=84)

---
+++ lowerbound.java
@@ -0,0 +1,20 @@
+//striver ato z lower bound
+class Solution {
+    public int lowerBound(int[] nums, int x) {
+       int n=nums.length;
+       int low=0;
+       int high=n-1;
+       int ans=n;
+       while(low<=high){
+       int  mid=(low+high)/2;
+        if(nums[mid]>=x) {
+            ans=mid;
+            high=mid-1;
+       }
+       else {
+        low=mid+1;
+       }
+       }
+       return ans;
+     }
+}
