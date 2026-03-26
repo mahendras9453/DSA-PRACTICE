e889cc2a44bce5094c5ed7e6ff0243a1b3979b7e
mahendras9453 <patelmahendra9152@gmail.com>
2026-03-26 20:11:38 +0530

    LongestSubstring(P=110)

---
+++ longestsubstring.java
@@ -0,0 +1,23 @@
+//leetcode(3) return the longest substring without repeating characters
+import java.util.*;
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n=s.length();
+        Set<Character> set= new HashSet<>();
+      
+        int left=0;
+        int Maxlength=0;
+        for(int right=0;right<n; right++){
+            while(set.contains(s.charAt(right))){
+                set.remove(s.charAt(left));
+                left++;
+            }
+            
+                set.add(s.charAt(right));
+                
+                Maxlength=Math.max(Maxlength,right-left+1);
+            
+        }
+        return Maxlength;
+    }
+}
