8ec77b76fcf9b1fc46edfa83c73e590dc92f62bd
mahendras9453 <patelmahendra9152@gmail.com>
2026-04-07 18:34:01 +0530

    MatrixSaerchII(P=129)

---
+++ matrixsearchII.java
@@ -0,0 +1,13 @@
+// leetcode(240)  matrix search II
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+      int r=0;
+      int c=matrix[0].length-1;
+      while(r<matrix.length && c>=0){
+        if(matrix[r][c]==target) return true;
+        else if(matrix[r][c]<target) r++;
+        else c--;
+      }
+      return false;
+    }
+}
