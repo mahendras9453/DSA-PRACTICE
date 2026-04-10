631ec8021a3519ef43b8a00d6c3c932770326ac1
mahendras9453 <patelmahendra9152@gmail.com>
2026-04-10 21:36:10 +0530

    InsertionAtBeginingofLL(P=133)

---
+++ LLinsertAtB.java
@@ -0,0 +1,19 @@
+// GFG problem insertion At begining of Linked list
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+
+class Solution {
+    public Node insertAtFront(Node head, int x) {
+       Node newNode=new Node(x);
+      newNode.next=head;
+        return newNode;
+    }
+}
