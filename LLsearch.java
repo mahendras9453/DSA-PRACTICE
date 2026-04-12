9307b1a27cc4a3d870bc0d38e34c3530cdc7fbe9
mahendras9453 <patelmahendra9152@gmail.com>
2026-04-12 18:37:36 +0530

    LLsearch(P=140)

---
+++ LLsearch.java
@@ -0,0 +1,19 @@
+// GFg search key in LL
+  class Node {
+   int data;
+    Node next;
+
+    Node(int d)  { data = d;  next = null; }
+}
+
+class Solution {
+    public boolean searchKey(Node head, int key) {
+      Node temp = head;
+      while(temp!=null){
+          if(temp.data==key) return true;
+          temp=temp.next;
+      }
+      return false;
+        
+    }
+}
