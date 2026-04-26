682df5be0fabba2152fbae959ee7b5180a740bb4
mahendras9453 <patelmahendra9152@gmail.com>
2026-04-26 20:39:08 +0530

    LLAddTwoNum(P=162)

---
+++ LLAddtwonum.java
@@ -0,0 +1,41 @@
+// leetcode (2)  Add two numbers
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+      ListNode dummy=new ListNode(-1);
+      ListNode tail=dummy;
+      int carry=0;
+      while(l1!=null || l2!=null || carry!=0){
+        int val1=0;
+        int val2=0;
+        if(l1!=null){
+            val1=l1.val;
+            l1=l1.next;
+        }
+        if(l2!=null){
+            val2=l2.val;
+            l2=l2.next;
+        }
+         int sum=val1+val2+carry;
+         int digit=sum;
+         if(sum>9){
+            carry=sum/10;
+            digit=sum%10;
+         }
+         else carry=0;
+         tail.next=new ListNode(digit);
+         tail=tail.next;
+         
+      }
+      return dummy.next;
+    }
+}
