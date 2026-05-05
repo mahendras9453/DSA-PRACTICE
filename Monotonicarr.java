6ea1238c415c06b591d4cb26bd057854d46b4460
mahendras9453 <patelmahendra9152@gmail.com>
2026-05-05 22:05:41 +0530

    Monotonicarr(P=171)

---
+++ Monotonicarr.java
@@ -0,0 +1,17 @@
+// leetcode(896)  Monotonic arr
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        boolean increasing =true;
+         boolean decreasing =true;
+
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]<nums[i+1]) {
+                decreasing=false;
+            }
+            else if(nums[i]>nums[i+1]){
+                increasing =false;
+            }
+        }
+        return increasing || decreasing;
+    }
+}
