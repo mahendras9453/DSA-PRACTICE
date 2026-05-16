a62d60f0c7479e1aafa0920279fae5ba8928e17f
mahendras9453 <patelmahendra9152@gmail.com>
2026-05-16 22:17:29 +0530

    Checkif2stringarrareequivalent(P=182)

---
+++ TwoStringequal.java
@@ -0,0 +1,16 @@
+// leetcode(1662) Check if Two string Arrays are equivalent
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+       StringBuilder sb1= new StringBuilder();
+       StringBuilder sb2= new StringBuilder();
+       for(int i=0;i<word1.length;i++){
+        sb1.append(word1[i]);
+       }
+       for(int i=0;i<word2.length;i++){
+        sb2.append(word2[i]);
+       }
+        
+
+       return sb1.toString().equals(sb2.toString()); 
+    }
+}
