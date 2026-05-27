297b8c6e1ad1769e9eeda9ca65f3c67883c088cf
mahendras9453 <patelmahendra9152@gmail.com>
2026-05-27 09:56:43 +0530

    PowerofTwo(P=195)

---
+++ PowerofTwo.java
@@ -0,0 +1,13 @@
+// leetcode(231)   Power Of Two 
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n==1) return true;
+        if(n<=0) return false;
+        if(n%2!=0) return false;
+       return  isPowerOfTwo(n/2);
+
+        
+    }
+    
+    
+}
