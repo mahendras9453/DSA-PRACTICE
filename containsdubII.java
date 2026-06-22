b253aea03d54f31395135f2f01c080d7c425bcba
mahendras9453 <patelmahendra9152@gmail.com>
2026-06-22 20:49:00 +0530

    ContainsDublicateII(P=214)

---
+++ containsdubII.java
@@ -0,0 +1,20 @@
+// leetcode (219) Contains Dublicate II
+import java.util.*;
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> set= new HashSet<>();
+
+        int n= nums.length;
+        for(int i=0;i<n;i++){
+            if(set.contains(nums[i])) return true;
+            else{
+                set.add(nums[i]);
+            }
+            if(set.size()>k){
+                set.remove(nums[i-k]);
+            }
+        }
+       
+        return false;
+    }
+}
