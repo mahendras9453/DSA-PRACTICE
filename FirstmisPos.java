6d3023acb6a3c94563f502b2bf22775e99b4be02
mahendras9453 <patelmahendra9152@gmail.com>
2026-06-25 12:42:44 +0530

    FirstMissingPositivenumber(P=216)

---
+++ FirstmisPos.java
@@ -0,0 +1,34 @@
+// leetcode 41 First missing Positive Number
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n= nums.length;
+        int contains=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                contains++;
+                break;
+
+            }
+        }
+        if(contains==0) return 1;
+           for(int i=0;i<nums.length;i++){
+            if(nums[i]<=0 || nums[i]>nums.length){
+                nums[i]=1;
+            }
+           }
+           for(int i=0;i<nums.length;i++){
+            int a=Math.abs(nums[i]);
+            if(a==nums.length){
+                nums[0]=-Math.abs(nums[0]);
+            }
+            else{
+                nums[a]=-Math.abs(nums[a]);
+            }
+           }
+           for(int i=1;i<n;i++){
+            if(nums[i]>0) return i;
+           }
+           if(nums[0]>0) return n;
+           return n+1;
+    }
+}
