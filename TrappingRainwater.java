0847da34f6aeba6714b2c598b9b4732e748caa64
mahendras9453 <patelmahendra9152@gmail.com>
2026-06-29 12:52:18 +0530

    Trapping Rain Water(P=221)

---
+++ TrappingRainwater.java
@@ -0,0 +1,23 @@
+// leetcode 11 Trapping the rain water
+import java.util.*;
+class Solution {
+    public int trap(int[] height) {
+      int left=  0;
+      int right = height.length-1;
+      int total=0;
+      int leftMax= 0;
+      int rightMax=0;
+      while (left < right) {
+            if (height[left] < height[right]) {
+                leftMax =Math.max(leftMax, height[left]);
+                total +=leftMax - height[left];
+                left++; 
+            } else {
+                rightMax =Math.max(rightMax, height[right]);
+                total +=rightMax- height[right];
+                right--; 
+            }
+        }
+           return total;
+    }
+}
