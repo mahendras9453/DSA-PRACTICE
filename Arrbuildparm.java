07294694b2b385dbcf53cbd452ccf2d8e40cd878
mahendras9453 <patelmahendra9152@gmail.com>
2026-07-22 19:50:19 +0530

    Buildarrfrompermution(P=223)

---
+++ Arrbuildparm.java
@@ -0,0 +1,21 @@
+// leetcode (1920) Build ar from Parmutatioin
+class Solution {
+    public int countSubstrings(String s) {
+     int ans=0;
+     for(int i=0;i<s.length();i++){
+        ans+=checkp(s,i,i);
+        ans+=checkp(s,i,i+1);
+
+     }
+     return ans;
+    }
+    public int  checkp(String s,int left, int right){
+        int count=0;
+        while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)){
+            left--;
+            right++;
+            count++;
+        }
+        return count;
+    }
+}
