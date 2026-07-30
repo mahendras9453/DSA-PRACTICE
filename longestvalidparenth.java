26a6f7712efc13ee84d17cde8530c9a29066b45c
mahendras9453 <patelmahendra9152@gmail.com>
2026-07-30 22:16:56 +0530

    Longestvalidparenth(P=229)

---
+++ longestvalidparenth.java
@@ -0,0 +1,23 @@
+// leetcode (32) Longest valid parethesis
+class Solution {
+    public int longestValidParentheses(String s) {
+        int maxlength=0;
+        Stack<Integer> st= new  Stack<>();
+        st.push(-1);
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                st.push(i);
+            }
+            else{
+                st.pop();
+                if(st.isEmpty()){
+                    st.push(i);
+                }
+                else{
+                    maxlength=Math.max(maxlength,i-st.peek());
+                }
+            }
+        }
+        return maxlength;
+    }
+}
