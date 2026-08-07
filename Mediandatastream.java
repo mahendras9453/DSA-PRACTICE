b8aebd42947ae1ba35899e34d79fef58b278c16c
mahendras9453 <patelmahendra9152@gmail.com>
2026-08-07 14:01:02 +0530

    Fimd Median from Data Stream(P=240)

---
+++ Mediandatastream.java
@@ -0,0 +1,26 @@
+// leetcode 295  Find Median from Data Stream
+class MedianFinder {
+     private  PriorityQueue<Integer> lo= new PriorityQueue<>((a,b) -> b-a);
+       private  PriorityQueue<Integer> hi= new PriorityQueue<>();
+     
+    
+    
+    public void addNum(int num) {
+        lo.offer(num);
+        hi.offer(lo.poll());
+        if(lo.size()<hi.size()){
+            lo.offer(hi.poll());
+        }
+    }
+    
+    public double findMedian() {
+        return lo.size()>hi.size() ? lo.peek() : (lo.peek()+ hi.peek())*0.5;
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
